22155071524ba4e5b068c3e746b0b6427c870759
John-Kim93 <dhkdhd123@naver,com>
2023-01-21 12:42:17 +0900

    call data

--- moglet_web_modules/next.config.js
+++ moglet_web_modules/next.config.js
@@ -1,7 +1,27 @@
-// @type {import('next').NextConfig}
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  env: {
+    ORIGINAL_DATA: `https://${process.env.ORIGINAL_DATA}`,
+    CONVERT_DATA: `https://${process.env.CONVERT_DATA}`,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.API,
+      },
+      {
+        source: "/convert/:filename*",
+        destination: `https://${process.env.CONVERT_DATA}:filename*`,
+      },
+      {
+        source: "/original/:filename*",
+        destination: `https://${process.env.ORIGINAL_DATA}:filename*`,
+      },
+    ];
   },
 }
 
